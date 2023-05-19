4c02e757cfa6c369eec26fad5df0a71aaa932cfa
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-19 20:56:44 -0300

    config inicial banco de dados

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,19 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = fastify()
-const PORT = 3001
+const prisma = new PrismaClient()
+const PORT = process.env.SERVER_PORT
+  ? parseInt(process.env.SERVER_PORT, 10)
+  : 3001
 
-app.get('/hello', () => {
-  return 'Hello World!'
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 
 app
