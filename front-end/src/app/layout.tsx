4a2bf6954e5e5d0f67c41a215a9688c86492d82d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-25 15:04:29 -0300

    feat: adiciona as stripes da aplicação

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -25,8 +25,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
         <main className="grid min-h-screen grid-cols-2">
-          <section className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+          <section className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-cover px-28 py-16">
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
           </section>
 
           {children}
