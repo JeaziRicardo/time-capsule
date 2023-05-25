cfa6a3ea8f7e59b3d55f5a2415c928782da6d35f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-25 18:05:35 -0300

    feat: cria o componente Hero

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { ReactNode } from 'react'
+import { Hero } from '@/components/Hero'
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
@@ -28,6 +29,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <section className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
             <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            <Hero />
           </section>
           <section className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
