3a66cb0343227fe00d499fdb8cd85ea20b7b2b61
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-25 17:01:02 -0300

    feat: cria o componente EmptyMemories

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,5 @@
+import EmptyMemories from '@/components/EmptyMemories'
+
 export default function Home() {
-  return (
-    <h1 className="bg-red-800 text-center text-2xl font-bold uppercase">
-      Hello World
-    </h1>
-  )
+  return <EmptyMemories />
 }
