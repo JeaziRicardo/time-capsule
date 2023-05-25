b51bf275744e03cf25fc762b83daa2a4e45876f1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-25 14:41:03 -0300

    feat: adiciona o blur da aplicação

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -24,7 +24,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <section className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+          </section>
+
+          {children}
+        </main>
       </body>
     </html>
   )
