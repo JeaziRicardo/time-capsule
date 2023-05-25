c22e48837759cb0b89829c338052923ea292667f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-25 18:18:07 -0300

    feat: cria o componente SignIn

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
 import { Copyright } from '@/components/Copyright'
+import { SignIn } from '@/components/SignIn'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -30,7 +31,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <section className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
             <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-
+            <SignIn />
             <Hero />
             <Copyright />
           </section>
