ebee09fe1e2e3d80cd8781903c5c67505388e0a8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-26 10:16:17 -0300

    feat: separa a aplicação em rotas

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,20 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import 'dotenv/config'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 const PORT = process.env.SERVER_PORT
   ? parseInt(process.env.SERVER_PORT, 10)
   : 3001
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
