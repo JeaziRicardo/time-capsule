027246e0ee08731bcd1d139cdd72320c9b59d6a5
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-05-27 13:23:01 -0300

    feat: cria a rota para registrar o user pelo github

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,17 +1,19 @@
-import fastify from 'fastify'
 import cors from '@fastify/cors'
 import 'dotenv/config'
+import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
-const app = fastify()
 const PORT = process.env.SERVER_PORT
   ? parseInt(process.env.SERVER_PORT, 10)
   : 3001
 
+const app = fastify()
 app.register(cors, {
   origin: true,
 })
 
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
